69400e7d27857a141c06167ea9baf324eea93af3
Way <webwatcharapong@gmail.com>
2026-04-29 14:16:54 +0700

    Fix: Add missing sharp import in gcs.js

--- Backend/utils/gcs.js
+++ Backend/utils/gcs.js
@@ -1,6 +1,7 @@
 import { Storage } from '@google-cloud/storage';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import sharp from 'sharp';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
